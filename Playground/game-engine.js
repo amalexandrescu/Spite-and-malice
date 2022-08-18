a90fc6d3739e1a36c19dfd1d96330ad850eaf638
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-08-18 18:30:05 +0300

    implemented cards to go

--- Playground/game-engine.js
+++ Playground/game-engine.js
@@ -1,20 +1,44 @@
 export class GameEngine {
-    deckOfCards;
+  deckOfCards;
 
-    constructor(customDeck) {
-        this.deckOfCards = customDeck ?? [1, 2, 3, 4];
-    }
+  constructor(customDeck) {
+    this.deckOfCards = customDeck ?? [1, 2, 3, 4];
+  }
 
-    getTopCard() {
-        return this.deckOfCards[this.deckOfCards.length - 1];
-    }
+  getTopCard() {
+    return this.deckOfCards[this.deckOfCards.length - 1];
+  }
 
-    removeTopCard() {
-        throw new Error("Here")
-        // return this.deckOfCards.pop();
-    }
+  removeTopCard() {
+    // throw new Error("Here")
+    return this.deckOfCards.pop();
+  }
 
-    addCardToDeck(card) {
-        this.deckOfCards.push(card);
-    }
+  addCardToDeck(card) {
+    this.deckOfCards.push(card);
+  }
+}
+
+export class CardsToGo {
+  remainingCards;
+
+  constructor(customRemainingCards) {
+    this.remainingCards = customRemainingCards;
+  }
+
+  getRemainingCards() {
+    return this.remainingCards;
+  }
+
+  countRemainingCards() {
+    return this.remainingCards.length;
+  }
+
+  getTopCard() {
+    return this.remainingCards[this.remainingCards.length - 1];
+  }
+
+  removeTopCard() {
+    return this.remainingCards.pop();
+  }
 }
--- Playground/game-engine.test.js
+++ Playground/game-engine.test.js
@@ -1,24 +1,55 @@
-import {GameEngine} from "./game-engine";
+import { GameEngine, CardsToGo } from "./game-engine";
 
-describe('game-engine', () => {
-    let gameEngine;
-    const customDeck = [10, 20];
+describe("game-engine", () => {
+  let gameEngine;
+  const customDeck = [10, 20];
 
-    beforeEach(() => {
-        gameEngine = new GameEngine(customDeck);
-    })
+  beforeEach(() => {
+    gameEngine = new GameEngine(customDeck);
+  });
 
-    it('should get top card', () => {
-        expect(gameEngine.getTopCard()).toEqual(20);
-    })
+  it("should get top card", () => {
+    expect(gameEngine.getTopCard()).toEqual(20);
+  });
 
-    it('should remove top card', () => {
-        gameEngine.removeTopCard();
-        expect(gameEngine.getTopCard()).toEqual(10);
-    })
+  it("should remove top card", () => {
+    gameEngine.removeTopCard();
+    expect(gameEngine.getTopCard()).toEqual(10);
+  });
 
-    it('should add card on top of deck', () => {
-        gameEngine.addCardToDeck(30);
-        expect(gameEngine.getTopCard()).toEqual(30);
-    })
-})
+  it("should add card on top of deck", () => {
+    gameEngine.addCardToDeck(30);
+    expect(gameEngine.getTopCard()).toEqual(30);
+  });
+});
+
+describe("CardsToGo", () => {
+  let cardsToGo;
+  const customDeck = [
+    { number: 7, suite: "diamonds" },
+    { number: 4, suite: "hearts" },
+  ];
+
+  beforeEach(() => {
+    cardsToGo = new CardsToGo(customDeck);
+  });
+
+  it("should initialize with deck of cards", () => {
+    expect(cardsToGo.getRemainingCards()).toEqual(customDeck);
+  });
+
+  it("should tell me how many cards are left", () => {
+    expect(cardsToGo.countRemainingCards()).toEqual(2);
+  });
+
+  it("should tell you what the top card is", () => {
+    expect(cardsToGo.getTopCard()).toEqual({ number: 4, suite: "hearts" });
+  });
+
+  it("you should be able to remove the top card", () => {
+    cardsToGo.removeTopCard();
+    expect(cardsToGo.getRemainingCards()).toEqual([
+      { number: 7, suite: "diamonds" },
+    ]);
+  });
+});
